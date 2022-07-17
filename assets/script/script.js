4463f44ee0332907100093070c656095fe507a94
Paul Power <power_604@msn.com>
2022-07-17 18:14:02 +0100

    Preliminary Content and Navigation Bar
    Nav bar added with pages / hero section  / brief content/ fonts added.

---
+++ assets/script/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListner('click', () => {
+  navbarLinks.classList.toggle('active')
+})
